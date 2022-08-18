9a52fe33e0b4533482eb2e2f3acb87173d6639f7
hyerriimm <110383614+hyerriimm@users.noreply.github.com>
2022-08-18 20:20:43 +0900

    Add files via upload

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Header() { // 맨 윗줄에 들어가는 헤더
+  return (
+    <div className="container">
+      <div>My Todo List</div>
+      <div>React</div>
+    </div>
+  );
+}
+export default Header;
